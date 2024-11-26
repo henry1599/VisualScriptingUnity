9f2eacd32339b7a7eadd602ec1df3d5150773d62
h3nri1599 <h3nri1599@gmail.com>
2024-11-26 18:06:31 +0700

    Update NodeDataBase.cs

--- hGraph/Assets/hGraph/Runtime/Data/NodeDataBase.cs
+++ hGraph/Assets/hGraph/Runtime/Data/NodeDataBase.cs
@@ -9,7 +9,54 @@ public enum eNodeType
     Function,
     Custom,
 }
+public enum eNodePortFlowType
+{
+    Entry,
+    Finish
+}
 
 public class NodeDataBase
 {
+    public eNodeType NodeType;
+    public string Name;
+    public string DisplayName;
+    public string Info;
+    public EntryPort Entry;
+    public FinishPort Finish;
+    public List<NodePortBase> InputPorts;
+    public List<NodePortBase> OutputPorts;
+    public NodeDataBase NodeFrom;
+    public NodeDataBase NodeTo;
+    public NodeDataBase()
+    {
+        Entry = new EntryPort();
+        Finish = new FinishPort();
+        InputPorts = new List<NodePortBase>();
+        OutputPorts = new List<NodePortBase>();
+        NodeFrom = null;
+        NodeTo = null;
+
+        Name = "New Node";
+        DisplayName = "New Node";
+        Info = "New Node Info";
+        NodeType = eNodeType.Custom;
+    }
+}
+
+public class NodePortBase
+{
+    public string Name;
+    public System.Type Type;
+}
+public class EntryPort : NodeFlowPortBase
+{
+    public override eNodePortFlowType FlowType => eNodePortFlowType.Entry;
+}
+public class FinishPort : NodeFlowPortBase
+{
+    public override eNodePortFlowType FlowType => eNodePortFlowType.Finish;
+}
+public abstract class NodeFlowPortBase
+{
+    public abstract eNodePortFlowType FlowType { get; }
 }
