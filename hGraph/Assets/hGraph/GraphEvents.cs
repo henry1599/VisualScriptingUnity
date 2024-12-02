af9af1775a331b332fd6b603a8906265047c6a3d
h3nri1599 <h3nri1599@gmail.com>
2024-12-02 11:51:57 +0700

    - Remove unnecessary resources

--- hGraph/Assets/hGraph/GraphEvents.cs
+++ hGraph/Assets/hGraph/GraphEvents.cs
@@ -5,6 +5,4 @@ using UnityEngine;
 
 public static class GraphEvents
 {
-    // * Node type (Class, Variable, Property, Function, Custom), Node name, Node type
-    public static Action<eNodeType, string, Type> ON_TOOLBOX_ITEM_CLICKED;
 }
