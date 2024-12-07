e7f4687246ee554987ea2bc6b90e3df0b3d5c303
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2024-12-07 22:15:05 +0700

    + Add unity visual scripting

--- hGraph/Assets/uNode3/Core.Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("uNode3.Editor")]
-[assembly: InternalsVisibleTo("uNode3.Pro.Editor")]
--- hGraph/Assets/uNode3/Core/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("uNode3.Core.Editor")]
-[assembly: InternalsVisibleTo("uNode3.Editor")]
-[assembly: InternalsVisibleTo("uNode3.Compiler")]
-[assembly: InternalsVisibleTo("uNode3.Pro.Editor")]
