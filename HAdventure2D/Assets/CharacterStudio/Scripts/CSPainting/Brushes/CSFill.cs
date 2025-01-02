41dc4560c49b9322d7e55f251aa9bf272900aa9d
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2025-01-03 00:48:16 +0700

    + Add fill tool

--- HAdventure2D/Assets/CharacterStudio/Scripts/CSPainting/Brushes/CSFill.cs
+++ HAdventure2D/Assets/CharacterStudio/Scripts/CSPainting/Brushes/CSFill.cs
@@ -32,7 +32,45 @@ namespace CharacterStudio
         }
         private void Fill(CSPaintingRenderer renderer, Vector2Int index, Color color)
         {
-
+            List<Vector2Int> visited = new();
+            Queue<Vector2Int> queue = new();
+            queue.Enqueue(index);
+            visited.Add(index);
+            Color targetColor = renderer.PixelColors[index.y * renderer.RT.width + index.x];
+            while (queue.Count > 0)
+            {
+                Vector2Int current = queue.Dequeue();
+                renderer.PixelColors[current.y * renderer.RT.width + current.x] = color;
+                var neighbors = GetNeighbors(renderer, current);
+                foreach (var neighbor in neighbors)
+                {
+                    if (visited.Contains(neighbor))
+                        continue;
+                    if (renderer.PixelColors[neighbor.y * renderer.RT.width + neighbor.x] == targetColor)
+                    {
+                        queue.Enqueue(neighbor);
+                        visited.Add(neighbor);
+                    }
+                }
+            }
+            renderer.UpdateRenderTexture();
+        }
+        List<Vector2Int> GetNeighbors(CSPaintingRenderer renderer, Vector2Int index)
+        {
+            List<Vector2Int> result = new();
+            Vector2Int top = index + Vector2Int.up;
+            Vector2Int bottom = index + Vector2Int.down;
+            Vector2Int left = index + Vector2Int.left;
+            Vector2Int right = index + Vector2Int.right;
+            if (top.y < renderer.RT.height)
+                result.Add(top);
+            if (bottom.y >= 0)
+                result.Add(bottom);
+            if (left.x >= 0)
+                result.Add(left);
+            if (right.x < renderer.RT.width)
+                result.Add(right);
+            return result;
         }
         public override void HandleCursor( bool isEnter )
         {
